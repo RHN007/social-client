aae1b33525295d74396392bb26abf6b49e340b8a
RHN007 <rakibul.nayon@outlook.com>
2022-12-30 22:06:24 +0600

    add

---
+++ src/firebase /firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCMkUo90umMrC1BrHXjfSywycUTZgLVFOM",
+    authDomain: "social-media-f6699.firebaseapp.com",
+    projectId: "social-media-f6699",
+    storageBucket: "social-media-f6699.appspot.com",
+    messagingSenderId: "181096908576",
+    appId: "1:181096908576:web:c24cd815d48c7397247945"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app; 
